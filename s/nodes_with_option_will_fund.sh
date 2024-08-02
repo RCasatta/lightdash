4b2c52beb6c2e55ed481cb894459bf72f0e70193
Riccardo Casatta <riccardo@casatta.it>
2024-08-02 13:47:51 +0200

    ./nodes_with_option_will_fund.sh

---
+++ s/nodes_with_option_will_fund.sh
@@ -0,0 +1,661 @@
+[
+  {
+    "nodeid": "020d1617e27ac022395352f2b3774969593d3d6ddff6fb117d820a9dda8da45217",
+    "alias": "i'llTryAnythingOnce",
+    "color": "d7cfad",
+    "last_timestamp": 1722478677,
+    "features": "080000000000000000000000000088a0080a8a59a1",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "23.88.103.101",
+        "port": 40009
+      },
+      {
+        "type": "torv3",
+        "address": "buqjxr6ha4oob7knppq2ky4urrtohruowpdpldjdapgn2k5gt2nx74id.onion",
+        "port": 9735
+      },
+      {
+        "type": "torv3",
+        "address": "b2abr4r5xjkxy32yj3eclnggyrywarau3cbc5tjshogqaopbdoywrayd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000,
+      "lease_fee_basis": 120,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 1000,
+      "channel_fee_max_proportional_thousandths": 10,
+      "compact_lease": "029a0078000a0000000203e8"
+    }
+  },
+  {
+    "nodeid": "0380ef0209ff1b46c38a37cd40f613d1dae3eba481a909459d6c1434a0e56e5d8c",
+    "alias": "noserver4u | CLN",
+    "color": "ff0000",
+    "last_timestamp": 1721002334,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "89.58.53.211",
+        "port": 9735
+      },
+      {
+        "type": "ipv6",
+        "address": "2a03:4000:69:d0b:d8e9:21ff:fe87:deeb",
+        "port": 9735
+      },
+      {
+        "type": "torv3",
+        "address": "qqeohecjxvdx6uu5zbqw342cdoqlvp6zdauu3562kau6hw6pwgwfurad.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 1000000,
+      "lease_fee_basis": 59,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 3,
+      "compact_lease": "029a003b0003000003e8"
+    }
+  },
+  {
+    "nodeid": "02d695b01c7a6909e716c863fb39bc5fb7bbdc3824b7fdce53adc593e5be080e73",
+    "alias": "MindlinerTre",
+    "color": "fec89a",
+    "last_timestamp": 1720798563,
+    "features": "080000000000000200000000000088aa882a8a59a1",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "62.12.168.100",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000,
+      "lease_fee_basis": 50,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 5000000,
+      "channel_fee_max_proportional_thousandths": 100,
+      "compact_lease": "029a00320064000000024c4b40"
+    }
+  },
+  {
+    "nodeid": "025ae40275ff32c9461ea7f65aa7ac8cdbf83814d2d9d2c402262fdcf69fd1f3d4",
+    "alias": "cguida",
+    "color": "dd6fd1",
+    "last_timestamp": 1720543847,
+    "features": "0200000000000088a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "3usuhkldvsmjedlhjdfy466wtlxgbclz5t7yw2zk53ve2ijevxghz4yd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 65,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 5000000,
+      "channel_fee_max_proportional_thousandths": 100,
+      "compact_lease": "029a00410064000007d04c4b40"
+    }
+  },
+  {
+    "nodeid": "03925ecce6de3ebcd5dfcc42c0eefc697ca136724225d4c32ee01b133cb87696c7",
+    "alias": "MinistryOfSillyWalks🚶",
+    "color": "beefee",
+    "last_timestamp": 1720466931,
+    "features": "88aa802a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "ta5bnelfswvvd46fx4ayisp64i6c2txvhix42l63xf7addoulbmw6yyd.onion",
+        "port": 9736
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 2,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 1000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a00020002000001f403e8"
+    }
+  },
+  {
+    "nodeid": "03fd3e0f2504e149b1907cab853712a213b182b8c972169fa3ad009b623533103f",
+    "alias": "MAGNUS",
+    "color": "7f7f7f",
+    "last_timestamp": 1722524721,
+    "features": "88a0002a0a69a2",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "6mr3znhifbeskpasbc6ochsmenj4enbymfyfdjaeqxybzqbetvyphcad.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 5,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a00050002000001f40186a0"
+    }
+  },
+  {
+    "nodeid": "0313bae3a7d09d7f1a7ff595918567ba39e503789884d003036a88f7fcea3ec66f",
+    "alias": "YELLOWWAFFLE",
+    "color": "baab04",
+    "last_timestamp": 1713798719,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "88.198.91.246",
+        "port": 9735
+      },
+      {
+        "type": "torv3",
+        "address": "yehizkxkj7s5arvdo7anem7u427e4julgmdbtliea23kaafguahi4dyd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 1000000,
+      "lease_fee_basis": 50,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 1,
+      "compact_lease": "029a00320001000003e8"
+    }
+  },
+  {
+    "nodeid": "0205198c099c45acedf988445f71da087ca39cd80847e53a7819e8f4e45768d7d3",
+    "alias": "LOOP tor-only",
+    "color": "020519",
+    "last_timestamp": 1722538364,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "4j5qyuorv73qnkx6rm6572bw3f2ishkliu2i6wlwuxbrd3srhe324fqd.onion",
+        "port": 9736
+      },
+      {
+        "type": "torv3",
+        "address": "4j5qyuorv73qnkx6rm6572bw3f2ishkliu2i6wlwuxbrd3srhe324fqd.onion",
+        "port": 9736
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 30000000,
+      "lease_fee_basis": 60,
+      "funding_weight": 27000,
+      "channel_fee_max_base_msat": 10000,
+      "channel_fee_max_proportional_thousandths": 100,
+      "compact_lease": "6978003c0064000075302710"
+    }
+  },
+  {
+    "nodeid": "0225e29bcf897f22d9f546ad71185f9519db538d484b2fd2ea2be02e20b5c7224b",
+    "alias": "ATLAS_ENLIGHTENED",
+    "color": "0225e2",
+    "last_timestamp": 1721108772,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "31.16.217.162",
+        "port": 9735
+      },
+      {
+        "type": "ipv6",
+        "address": "2a01:4f8:1c1c:ea1c:34f6:e60f:6298:6eea",
+        "port": 9735
+      },
+      {
+        "type": "torv3",
+        "address": "saq3ihcvzdyeqlrkyidgtqau456omvehyddxbqu5upx4nrf5nf5nb4id.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 25,
+      "funding_weight": 1000,
+      "channel_fee_max_base_msat": 1000,
+      "channel_fee_max_proportional_thousandths": 1,
+      "compact_lease": "03e800190001000001f403e8"
+    }
+  },
+  {
+    "nodeid": "03fef777d58a529df02a3fb267690e0c9033767b555cc1c63844bb2d3498789f91",
+    "alias": "Crypt04n4rkMoV",
+    "color": "ffff00",
+    "last_timestamp": 1720040820,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "2obm3yvfj5m3zabnea5y2xolimeuc4gbelika3pa7div5pk2eolqrtad.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 10,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 1250,
+      "channel_fee_max_proportional_thousandths": 1,
+      "compact_lease": "029a000a0001000007d004e2"
+    }
+  },
+  {
+    "nodeid": "0232a5c6b4e1f1dae5e50528fbc2f0de6f2d66363638c83cfab65391e3eff48ca8",
+    "alias": "Bitmynt",
+    "color": "0232a5",
+    "last_timestamp": 1722571924,
+    "features": "080000000000000000000000000088a0882a0a69a2",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "158.220.123.221",
+        "port": 9735
+      },
+      {
+        "type": "torv3",
+        "address": "hkg3bokiics7eoikasj5sla6czspphq7obube5dbjrz32ouoex6ut2yd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 65,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 5,
+      "compact_lease": "029a00410005000007d0"
+    }
+  },
+  {
+    "nodeid": "0273497dc6db975876b03ec12004e058f20ecd7ad81ceb2e15d357d7327893997c",
+    "alias": "24h PREMIUM LIQUIDITY",
+    "color": "027349",
+    "last_timestamp": 1721413891,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "kdzynlssh5g255c5swlhsblee3hyneqyuzbtztjf3itp3z5bdw26acyd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 65,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 10,
+      "compact_lease": "029a0041000a000007d00186a0"
+    }
+  },
+  {
+    "nodeid": "03e2408a49f07d2f4083a47344138ef89e7617e63919202c92aa8d49b574a560ae",
+    "alias": "🦉 BRUCEWAINE|CLN 🦉",
+    "color": "282e3c",
+    "last_timestamp": 1716623448,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "65.108.246.14",
+        "port": 19736
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 50,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a00320002000007d00186a0"
+    }
+  },
+  {
+    "nodeid": "0253029d39ca97c4d271af8926b4f3fec37a3aa87b5e538329845f2fc76146fa28",
+    "alias": "SLEEPYTOLL",
+    "color": "025302",
+    "last_timestamp": 1722467033,
+    "features": "88a0802a0a69a2",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "77.100.20.178",
+        "port": 9735
+      },
+      {
+        "type": "torv3",
+        "address": "v6fpuqvthutullww4ghjpjupe5el6acp37eiodzyiojvugmziablfryd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 50,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a00320002000001f40186a0"
+    }
+  },
+  {
+    "nodeid": "032f873d1fdbf0ee48b4c3aa0bdcf231922e455fd400c24d7275bc42653a92483d",
+    "alias": "Lightning_is_real_Bitcoin_Cash",
+    "color": "032f87",
+    "last_timestamp": 1716988268,
+    "features": "080000000000000000000000000088a0082a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "autc7qdwq32vdsvzgunmhmswlnyugwn4vbkbsewy3vobnvjsx4qnqqqd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 65,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 1,
+      "compact_lease": "029a00410001000007d0"
+    }
+  },
+  {
+    "nodeid": "034e91e9e5ae7053f2c07fe90205bbd4a9cd85cedc908f83316607e2802921ccf3",
+    "alias": "LightningNetwork",
+    "color": "ffffff",
+    "last_timestamp": 1722413045,
+    "features": "88a0802a0a69a2",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "axue7la5yx52catmq7tvirf7dd5nb2uysb35ame6jeup76bst6sijaid.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 65,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 1000000,
+      "channel_fee_max_proportional_thousandths": 10,
+      "compact_lease": "029a0041000a000007d00f4240"
+    }
+  },
+  {
+    "nodeid": "02cf19b8ff79dbbd76350ad6ed8ea46f2150f80dc790536442ebf206aef451f220",
+    "alias": "⚡ @leblitzdick ⚡",
+    "color": "02cf19",
+    "last_timestamp": 1721676866,
+    "features": "88a0000a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "femaa2jbd66fdlkremw2jpk2v3hynnbcdqajizfsjbdglcmqqp3rjqqd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 65,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 21,
+      "compact_lease": "029a00410015000007d0"
+    }
+  },
+  {
+    "nodeid": "03e0814803896c8b00e6a835f9daf3b78765e7a8d8e9985aabc512b3df43518fcb",
+    "alias": "and its gone",
+    "color": "03e081",
+    "last_timestamp": 1709541251,
+    "features": "88a0000a8a5961",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "vu6clsnbknpl4tdqygw37l2mgiar2r5qyljeg2ikgs5c4lfmpad6hkyd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 65,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 21,
+      "compact_lease": "029a00410015000007d0"
+    }
+  },
+  {
+    "nodeid": "0279f06eba0e1080f6a693201f090d0635a0e5dd2ef57d0207210e3d338133092e",
+    "alias": "SwissRouting🇨🇭",
+    "color": "d52b1e",
+    "last_timestamp": 1721385084,
+    "features": "88a088282a69a2",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "193.32.127.237",
+        "port": 55287
+      },
+      {
+        "type": "torv3",
+        "address": "scdesnle7un7ns3m4doocbfg6u6ovo4etbp4bb2vha3xju4hxp3ehcqd.onion",
+        "port": 9736
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 300000,
+      "lease_fee_basis": 33,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 3,
+      "compact_lease": "029a002100030000012c"
+    }
+  },
+  {
+    "nodeid": "0349d945229efc3ba1d37d2c21bef2295183906787eef79f4266993eb5efaee4e8",
+    "alias": "FedCoinBad",
+    "color": "062b5d",
+    "last_timestamp": 1719766533,
+    "features": "080000000000000000000000000088a0882a8a59a1",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "vrltunsopo5yyqszn4twzd636qvfqkodzw5xguiurbruyl5lf3hcseqd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 1776000,
+      "lease_fee_basis": 69,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 5000000,
+      "channel_fee_max_proportional_thousandths": 100,
+      "compact_lease": "029a00450064000006f04c4b40"
+    }
+  },
+  {
+    "nodeid": "02cca6c5c966fcf61d121e3a70e03a1cd9eeeea024b26ea666ce974d43b242e636",
+    "alias": "the_holy_cheese_grater",
+    "color": "02cca6",
+    "last_timestamp": 1714102752,
+    "features": "88a0802a0a69a2",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "45.55.129.100",
+        "port": 6666
+      },
+      {
+        "type": "ipv4",
+        "address": "104.131.77.55",
+        "port": 6666
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 60,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a003c0002000001f40186a0"
+    }
+  },
+  {
+    "nodeid": "03a7c61c056023c804c6d63693345e00ed8e2b28c8d2e0c455964bfff31128df40",
+    "alias": "InternationalRoutingService",
+    "color": "ffa500",
+    "last_timestamp": 1659157476,
+    "features": "80000028226aa2",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "h7a7yk4jixep6ozr7u2ehurh33kbznffzd4b5dsz7evoupexdh26lgqd.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 0,
+      "lease_fee_basis": 0,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 0,
+      "compact_lease": "029a0000000000000000"
+    }
+  },
+  {
+    "nodeid": "0378d144ce16dc11bb6584c4a397bcf241b60cfece20943faef10c4b72d3cd87d3",
+    "alias": "lightning-lars",
+    "color": "0378d1",
+    "last_timestamp": 1659621506,
+    "features": "80008028226aa2",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "2utlits6k3pyup4xhkmyxhyysvjosenyoupdtdouwcx33hxcwfcdtjid.onion",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 10,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a000a0002000001f40186a0"
+    }
+  },
+  {
+    "nodeid": "039b31301fa7a0f36194327071e07c43b6bbb12b0f01e6caaa2cc9e330f21eb9af",
+    "alias": "based58",
+    "color": "feac00",
+    "last_timestamp": 1713806299,
+    "features": "88a0802a8a59a1",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "45.55.129.100",
+        "port": 6668
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 60,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a003c0002000001f40186a0"
+    }
+  },
+  {
+    "nodeid": "031c0d8356994448e6859d7d767c792f8af4b86fe19d62e3431aba0488c0522931",
+    "alias": "kumulynja",
+    "color": "031c0d",
+    "last_timestamp": 1708246898,
+    "features": "88a0802a0269a2",
+    "addresses": [
+      {
+        "type": "ipv6",
+        "address": "2a02:a03f:8a99:9400:4d63:854f:c864:af86",
+        "port": 9735
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 2000000,
+      "lease_fee_basis": 50,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 100000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a00320002000007d00186a0"
+    }
+  },
+  {
+    "nodeid": "02c22bc3371ca350cddf8fba2fef3919d10a923ac9c6bfb1f4ac3e49818d7526ed",
+    "alias": "REMNET",
+    "color": "02c22b",
+    "last_timestamp": 1719497677,
+    "features": "88a0002a0269a2",
+    "addresses": [
+      {
+        "type": "ipv4",
+        "address": "5.75.184.195",
+        "port": 23406
+      },
+      {
+        "type": "torv3",
+        "address": "krg2p7arfofahcrqy5na6cvtwjudap3eq4lzeyv6p3eg6nlvj3saj2ad.onion",
+        "port": 9736
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 450000,
+      "lease_fee_basis": 18,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 80000,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a00120002000001c2013880"
+    }
+  },
+  {
+    "nodeid": "0275fa7a50101c63abe43e34d04e1878d5541324089dd28f722944e136fb611f37",
+    "alias": "Lightning2Careful",
+    "color": "0275fa",
+    "last_timestamp": 1705784408,
+    "features": "800088282269a2",
+    "addresses": [
+      {
+        "type": "torv3",
+        "address": "pmg3z37uktgemuj3sy3hfdgioorkond43ye6am26yb3tavfkaorlb3qd.onion",
+        "port": 9736
+      }
+    ],
+    "option_will_fund": {
+      "lease_fee_base_msat": 500000,
+      "lease_fee_basis": 50,
+      "funding_weight": 666,
+      "channel_fee_max_base_msat": 0,
+      "channel_fee_max_proportional_thousandths": 2,
+      "compact_lease": "029a00320002000001f4"
+    }
+  }
+]
