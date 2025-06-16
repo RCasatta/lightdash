db2c09ada4b8956a9a9dbaba143df6ef83186884
Riccardo Casatta <riccardo@casatta.it>
2025-06-16 10:48:30 +0200

    channels-closed

--- s/channels-closed.sh
+++ s/channels-closed.sh
@@ -1,1 +1,6 @@
-lightning-cli listclosedchannels | jq .closedchannels[].short_channel_id
+lightning-cli listnodes | jq '[.nodes[] | {nodeid, alias}]' >/tmp/nodes.json
+lightning-cli listclosedchannels | jq '[.closedchannels[] | {short_channel_id, peer_id}]' >/tmp/closed.json
+
+
+jq -s '[ .[0][] as $node | .[1][] | select(.peer_id == $node.nodeid) | {short_channel_id, peer_id, alias: $node.alias} ] | sort_by(.short_channel_id)' /tmp/nodes.json /tmp/closed.json
+
