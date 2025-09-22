e8ca45c2fd8cdd5ca9cfd40fad9fb440d4175c7c
Riccardo Casatta <riccardo@casatta.it>
2025-09-22 08:55:32 +0200

    initialize logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use env_logger::Env;
+use std::io::Write;
 
 use crate::store::Store;
 
@@ -34,6 +36,7 @@ enum Commands {
 }
 
 fn main() {
+    init_logging();
     let cli = Cli::parse();
     let store = Store::new();
 
@@ -52,4 +55,24 @@ fn main() {
             fees::run_fees(&store);
         }
     }
+}
+
+fn init_logging() {
+    let mut builder = env_logger::Builder::from_env(Env::default().default_filter_or("info"));
+    if let Ok(s) = std::env::var("RUST_LOG_STYLE") {
+        if s == "SYSTEMD" {
+            builder.format(|buf, record| {
+                let level = match record.level() {
+                    log::Level::Error => 3,
+                    log::Level::Warn => 4,
+                    log::Level::Info => 6,
+                    log::Level::Debug => 7,
+                    log::Level::Trace => 7,
+                };
+                writeln!(buf, "<{}>{}: {}", level, record.target(), record.args())
+            });
+        }
+    }
+
+    builder.init();
 }
